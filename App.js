bf777e24d1f273d40485ec2823589c1f6b78dec6
vadimstasiev <vadim444@hotmail.com>
2020-11-23 17:58:24 +0000

    implemented some ui

--- App.js
+++ App.js
@@ -1,9 +1,105 @@
 import React, { useState, useEffect } from "react";
 import { View } from "react-native";
 import auth from "@react-native-firebase/auth";
+import 'react-native-gesture-handler';
 
-import { Container, Text, Button, Content } from "native-base";
+
+import { Container, Header, Text, Form, Button, Item, Label, Input, Content } from "native-base";
 import * as Font from "expo-font";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Login')}>
+        <Text>Login Screen</Text>
+      </Button>
+    </View>
+  );
+}
+
+function LoginScreen({navigation}) {
+  return (
+    // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    //   <Text>Login Screen</Text>
+    //   <Button block light onPress={() => navigation.navigate('Home')}>
+    //     <Text>Go Home</Text>
+    //   </Button>
+      
+      
+    //   <Form>
+    //     <Item fixedLabel>
+    //       <Label>Username</Label>
+    //       <Input />
+    //     </Item>
+    //     <Item fixedLabel last>
+    //       <Label>Password</Label>
+    //       <Input />
+    //     </Item>
+        // <Button block info onPress={()=>{}}>
+        //   <Text>Login</Text>
+        // </Button>
+    //   </Form>
+
+      // <Button block info onPress={()=>{() => navigation.navigate('Register')}}>
+      //   <Text>Click to register instead</Text>
+      // </Button>
+    // </View>
+
+    <Container>
+        <Header />
+        <Content>
+          <Form>
+            <Item>
+              <Input placeholder="Email" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+          </Form>
+          <Button block info onPress={()=>{}}>
+            <Text>Login</Text>
+          </Button>
+          <Button block info onPress={() => navigation.navigate('Register')}>
+            <Text>Click to register instead</Text>
+          </Button>
+        </Content>
+      </Container>
+  );
+}
+
+function RegisterScreen({navigation}) {
+  return (
+    <Container>
+      <Header />
+      <Content>
+        <Form>
+          <Item>
+            <Input placeholder="Email" />
+          </Item>
+          <Item last>
+            <Input placeholder="Password" />
+          </Item>
+          <Item last>
+            <Input placeholder="Confirm Password" />
+          </Item>
+        </Form>
+        <Button block info onPress={()=>{}}>
+          <Text>Register</Text>
+        </Button>
+        <Button block info onPress={() => navigation.navigate('Login')}>
+          <Text>Click to login instead</Text>
+        </Button>
+      </Content>
+    </Container>
+  );
+}
+
+
+const Stack = createStackNavigator();
 
 function App() {
   // Set an initializing state whilst Firebase connects
@@ -23,16 +119,16 @@ function App() {
 
   if (initializing) return null;
 
+  let output;
   if (!user) {
-    return (
+    output = (
       <View>
         <Text>Login</Text>
       </View>
     );
-  }
-
-  return (
-    <View>
+  } else {
+    output = 
+      <View>
       <Text>Welcome {user.email}</Text>
       <Button block light>
         <Text>Light</Text>
@@ -56,7 +152,27 @@ function App() {
         <Text>Dark</Text>
       </Button>
     </View>
-  );
+  }
+  // return <NavigationContainer>{output}</NavigationContainer>
+  return (
+    // For performance optimizations
+    // please refer to https://reactnavigation.org/docs/hello-react-navigation/#passing-additional-props
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"  screenOptions={{title: 'JournaliZZe'}}>
+        <Stack.Screen name="Home">
+          {props => <HomeScreen {...props} extraData={{}} />}
+        </Stack.Screen>
+        <Stack.Screen name="Login">
+          {props => <LoginScreen {...props} extraData={{}} />}
+        </Stack.Screen>
+        <Stack.Screen name="Register">
+          {props => <RegisterScreen {...props} extraData={{}} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 export default App;
+
+
