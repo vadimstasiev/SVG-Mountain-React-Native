3a212688aa9e755bef7d4fee86375ed2cc683d0d
vadimstasiev <vadim444@hotmail.com>
2020-11-24 16:08:31 +0000

    adapting old code

--- App.js
+++ App.js
@@ -5,10 +5,16 @@ import 'react-native-gesture-handler';
 
 
 import { Container, Header, Text, Form, Button, Item, Label, Input, Content, Icon } from "native-base";
-import * as Font from "expo-font";
+
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+
+// import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/RegisterScreen';
+
 
 function HomeScreen({navigation}) {
   return (
@@ -21,97 +27,9 @@ function HomeScreen({navigation}) {
   );
 }
 
-function LoginScreen({navigation}) {
-  return (
-    <Container>
-        <Header />
-        <Content>
-          <Form>
-            <Item>
-              <Input placeholder="Email" />
-            </Item>
-            <Item last>
-              <Input placeholder="Password" />
-            </Item>
-          </Form>
-          <Button block info onPress={()=>{}}>
-            <Text>Login</Text>
-          </Button>
-          <Button block info onPress={() => navigation.navigate('Register')}>
-            <Text>Click to register instead</Text>
-          </Button>
-        </Content>
-      </Container>
-  );
-}
 
-const RegisterScreen = ({navigation}) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const registerUser =()=>{
-    if (confirmPassword===password){
-      auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(() => {
-        console.log('User account created & signed in!');
-      })
-      .catch(error => {
-        if (error.code === 'auth/email-already-in-use') {
-          console.log('That email address is already in use!');
-        }
-        
-        if (error.code === 'auth/invalid-email') {
-          console.log('That email address is invalid!');
-        }
-        console.error(error);
-      });
-    }
-  }
-  const validateEmail = () => {
-    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
-      console.log("You have entered a valid email!")
-    } else {
-      console.log("You have entered an invalid email address!")
-    }
-  }
-  function checkPassword(e) { 
-    const PASSWORD_RULES=  /^[A-Za-z]\w{7,35}$/;
-    console.log(e.target.getNativeRef());
-    if (password.match(PASSWORD_RULES)) { 
-      console.log('valid password')
-      return true;
-    } else { 
-      console.log('Please enter a Password from 8 to 35 digits long')
-      return false;
-    }
-  }
-  return (
-    <Container>
-      <Header />
-      <Content>
-        <Form>
-          <Item>
-            <Input placeholder="Email" onChangeText={email=> {setEmail(email)}} onEndEditing={validateEmail}/>
-          </Item>
-          <Item last>
-            <Input placeholder="Password" onChangeText={password=> {setPassword(password)}} onEndEditing={checkPassword} />
-          </Item>
-          <Item last>
-            <Input placeholder="Confirm Password" onChangeText={confirmPassword=> {setConfirmPassword(confirmPassword)}}/>
-            {/* <Icon name='close-circle' /> */}
-          </Item>
-        </Form>
-        <Button block info onPress={registerUser}>
-          <Text>Register</Text>
-        </Button>
-        <Button block info onPress={() => navigation.navigate('Login')}>
-          <Text>Click to login instead</Text>
-        </Button>
-      </Content>
-    </Container>
-  );
-}
+
+
 
 const Stack = createStackNavigator();
 
